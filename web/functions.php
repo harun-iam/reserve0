50bd6f7e219b66cc2ebe3e343728cea2d1c00f53
harun-iam <wildspeed8610running@gmail.com>
2023-08-24 15:33:19 +0900

    日時を適切なフォーマットで表示

--- web/functions.php
+++ web/functions.php
@@ -1,9 +1,11 @@
 <?php
+//引数で与えられた配列を元にプルダウンリストを生成する
 function arrayToSelect($inputName, $srcArray, $selectedIndex = "") {
 
   $temphtml = "<select class=\"form-select\" name=\"{$inputName}\">" . PHP_EOL;
 
   foreach ($srcArray as $key => $val) {
+    //キーと選択値を比較して一致したらselectedをつける
     if ($key == $selectedIndex) {
       $selectedText = "selected";
     } else {
@@ -16,5 +18,11 @@ function arrayToSelect($inputName, $srcArray, $selectedIndex = "") {
   $temphtml .= "</select>" . PHP_EOL;
 
   return $temphtml;
+}
+
+//引数で与えたられた日時を表示形式に返還する
+function format_date($yyyymmdd) {
+  $week = array('日','月','火','水','木','金','土',);
+  return date('n/j('.$week[date('w', strtotime($yyyymmdd))] .')', strtotime($yyyymmdd));
 }
 
